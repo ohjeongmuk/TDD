04dba2efa129f81cbf1ec433a8a7d19e1fa48ecd
ohjeongmuk <ojmh011@naver.com>
2023-05-14 10:23:51 -0700

    Lower

--- check_pwd.py
+++ check_pwd.py
@@ -1,5 +1,6 @@
 def check_pwd(pwd):
     is_lower = False
+    is_upper = False
 
     if len(pwd) < 8 or len(pwd) > 20:
         return False
@@ -12,5 +13,12 @@ def check_pwd(pwd):
     if is_lower == False:
         return False
 
+    # check there is at least one UpperCase
+    for i in pwd:
+        if i.isupper():
+            is_upper= True
+            break
+    if is_upper == False:
+        return False
 
     return True
--- tests.py
+++ tests.py
@@ -3,13 +3,13 @@ from check_pwd import check_pwd
 
 class TestCase(unittest.TestCase):
     def test1(self):
-        # len(pwd) == 7
+        # len(pwd) == 7 False
         pwd = "Asdfg1!"
         self.assertFalse(check_pwd(pwd))
 
     def test2(self):
-        # len(pwd) == 10
-        pwd = "aaaaaaaaa1"
+        # len(pwd) == 10 True
+        pwd = "Aaaaaaaaa1"
         self.assertFalse(check_pwd(pwd))
 
     def test3(self):
@@ -18,13 +18,18 @@ class TestCase(unittest.TestCase):
         self.assertFalse(check_pwd(pwd)) 
 
     def test4(self):
-        # len(pwd) == 8 & All lower
+        # len(pwd) == 8 & All Upper
         pwd = "ABCDEFGH"
         self.assertFalse(check_pwd(pwd)) 
     
     def test5(self):
-        # len(pwd) == 7 & All upper
+        # len(pwd) == 7 & All Lower
         pwd = "asdfghjs"
+        self.assertFalse(check_pwd(pwd)) 
+
+    def test6(self):
+        # len(pwd) == 10
+        pwd = "AAAAAAaAA"
         self.assertFalse(check_pwd(pwd)) 
 
 
